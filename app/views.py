3e896c69bb60daa09c74957658c08b8e6071dbac
iUlloaRod <i.ulloarod@gmail.com>
2021-06-26 10:44:54 -0400

    con text activities funcionando pero con errores

--- app/views.py
+++ app/views.py
@@ -22,20 +22,33 @@ def reset(request):
 
 def process_money(request):
     if request.method == "POST":
+        if "activities" not in request.session:
+            request.session["activities"]=[]
+        time= strftime("%d de %B de %Y %H:%M:%S %p", localtime())
         lugar=request.POST["button"]
         if lugar == "farm":
             earnings = random.randint(10, 20)
             request.session["gold"] = request.session["gold"] + earnings
+            result = (f"Earned {earnings} golds from {lugar}! {time}" )
+            request.session["activities"].append(result)
+            print(result)
         elif lugar == "cave":
             earnings = random.randint(5, 10)
             request.session["gold"] = request.session["gold"] + earnings
+            result = (f"Earned {earnings} golds from {lugar}! {time}" )
+            print(result)
         elif lugar == "house":
             earnings = random.randint(2, 5)
             request.session["gold"] = request.session["gold"] + earnings
+            result = (f"Earned {earnings} golds from {lugar}! {time}" )
+            print(result)
         elif lugar == "casino":
             earnings = random.randint(-50, 50)
             request.session["gold"] = request.session["gold"] + earnings
+            result = (f"Earned {earnings} golds from {lugar}! {time}" )
+            print(result)
     context={
         "gold" : request.session["gold"],
+        "result" : request.session["activities"],
     }
     return render(request, "index.html", context)
