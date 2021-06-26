6183cf3f0b2e24e3bc78819b8e3d512b1e2fc39e
iUlloaRod <i.ulloarod@gmail.com>
2021-06-25 23:00:57 -0400

    cantidades aleatorias de oro funcionando. falta registro

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 import random
-
+from random import randint
+from time import strftime, localtime
+import locale
+locale.setlocale(locale.LC_ALL, 'es-ES')
 
 def index(request):
     if "gold" not in request.session:
@@ -11,14 +14,28 @@ def index(request):
     return render(request, "index.html", context)
 
 
-def process_money(request):
-    ran_val = random.randint(0, 50)
-    if request.method == "POST":
-        request.session["gold"] = request.session["gold"] + ran_val
-    return redirect("/")
-
-
 def reset(request):
     if request.method == "GET":
         request.session["gold"]=0
     return redirect("/")
+
+
+def process_money(request):
+    if request.method == "POST":
+        lugar=request.POST["button"]
+        if lugar == "farm":
+            earnings = random.randint(10, 20)
+            request.session["gold"] = request.session["gold"] + earnings
+        elif lugar == "cave":
+            earnings = random.randint(5, 10)
+            request.session["gold"] = request.session["gold"] + earnings
+        elif lugar == "house":
+            earnings = random.randint(2, 5)
+            request.session["gold"] = request.session["gold"] + earnings
+        elif lugar == "casino":
+            earnings = random.randint(-50, 50)
+            request.session["gold"] = request.session["gold"] + earnings
+    context={
+        "gold" : request.session["gold"],
+    }
+    return render(request, "index.html", context)
