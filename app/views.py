18e7452d25123d0bb924185da6928e04843072b4
iUlloaRod <i.ulloarod@gmail.com>
2021-06-28 13:48:37 -0400

    codigo embellecido, pero activities todavia es feaso

--- app/views.py
+++ app/views.py
@@ -6,49 +6,47 @@ import locale
 locale.setlocale(locale.LC_ALL, 'es-ES')
 
 def index(request):
+#creo una variable "gold" donde guardo el valor del oro obtenido en el proceso y le doy valor "0"´para comenzar. 
+# Lo dejo en "context" para poder llamarlo en el index
     if "gold" not in request.session:
         request.session["gold"]=0
+    if "activities" not in request.session:
+        request.session["activities"]=[]
     context={
         "gold" : request.session["gold"],
+        "activities" : request.session["activities"],
     }
     return render(request, "index.html", context)
-
-
-def reset(request):
-    if request.method == "GET":
-        request.session["gold"]=0
-    return redirect("/")
 
 
 def process_money(request):
-    if request.method == "POST":
-        if "activities" not in request.session:
-            request.session["activities"]=[]
-        time= strftime("%d de %B de %Y %H:%M:%S %p", localtime())
-        lugar=request.POST["button"]
-        if lugar == "farm":
-            earnings = random.randint(10, 20)
-            request.session["gold"] = request.session["gold"] + earnings
-            result = (f"Earned {earnings} golds from {lugar}! {time}" )
-            request.session["activities"].append(result)
-            print(result)
-        elif lugar == "cave":
-            earnings = random.randint(5, 10)
-            request.session["gold"] = request.session["gold"] + earnings
-            result = (f"Earned {earnings} golds from {lugar}! {time}" )
-            print(result)
-        elif lugar == "house":
-            earnings = random.randint(2, 5)
-            request.session["gold"] = request.session["gold"] + earnings
-            result = (f"Earned {earnings} golds from {lugar}! {time}" )
-            print(result)
-        elif lugar == "casino":
-            earnings = random.randint(-50, 50)
-            request.session["gold"] = request.session["gold"] + earnings
-            result = (f"Earned {earnings} golds from {lugar}! {time}" )
-            print(result)
-    context={
-        "gold" : request.session["gold"],
-        "result" : request.session["activities"],
-    }
-    return render(request, "index.html", context)
+    time= strftime("%d of %B of %Y %H:%M:%S %p", localtime())
+    place=request.POST["button"]
+    if place == "farm":
+        earnings = random.randint(10, 20)
+        request.session["gold"] = request.session["gold"] + earnings
+        result = (f"Earned {earnings} gold from {place}! {time}" )
+        request.session["activities"].append(result)
+    elif place == "cave":
+        earnings = random.randint(5, 10)
+        request.session["gold"] = request.session["gold"] + earnings
+        result = (f"Earned {earnings} gold from {place}! {time}" )
+        request.session["activities"].append(result)
+    elif place == "house":
+        earnings = random.randint(2, 5)
+        request.session["gold"] = request.session["gold"] + earnings
+        result = (f"Earned {earnings} gold from {place}! {time}" )
+        request.session["activities"].append(result)
+    elif place == "casino":
+        earnings = random.randint(-50, 50)
+        request.session["gold"] = request.session["gold"] + earnings
+        result = (f"Earned {earnings} gold from {place}! {time}" )
+        request.session["activities"].append(result)
+    return redirect("/")
+
+def reset(request):
+    if "gold" in request.session:
+        del request.session["gold"]
+    if "activities" in request.session:
+        del request.session["activities"]
+    return redirect("/")
